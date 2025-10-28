0f5e558b5bfabbbdf7ef20550ddf4dd32b71d496
DIASBruno44 <dias_bruno@outlook.fr>
2025-10-28 14:58:55 +0100

    Add logo convertisseur

---
+++ IntroductionProject/MeteoApp/MeteoApp.WPF/Converters/MeteoIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MeteoApp.WPF.Converters
+{
+    // Convertit le code d'icône de l'API (ex: "04d") en une URL d'image complète
+    public class MeteoIconConverter : IValueConverter
+    {
+        private const string BaseIconUrl = "http://openweathermap.org/img/wn/";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Vérifie si la valeur est une chaîne (le code de l'icône)
+            if (value is string iconCode && !string.IsNullOrEmpty(iconCode))
+            {
+                return new Uri($"{BaseIconUrl}{iconCode}@2x.png");
+            }
+
+            // Retourne null si le code est manquant
+            return null;
+        }
+
+        // Non utilisé pour ce cas, mais obligatoire pour IValueConverter
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
